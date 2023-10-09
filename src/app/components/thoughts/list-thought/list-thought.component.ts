e5fd43290a2fc8ce27dea7ae252e83ddf5bda44f
Roberto Costa <roberto.araujo339@gmail.com>
2023-10-09 11:04:11 -0300

    thought component created

--- src/app/components/thoughts/list-thought/list-thought.component.ts
+++ src/app/components/thoughts/list-thought/list-thought.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-list-thought',
   templateUrl: './list-thought.component.html',
-  styleUrls: ['./list-thought.component.css']
+  styleUrls: ['./list-thought.component.css'],
 })
 export class ListThoughtComponent implements OnInit {
+  listaPensamentos = [
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo3',
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @Input()',
+      autoria: 'Componente filho',
+      modelo: 'modelo2',
+    },
+    {
+      conteudo:
+        'Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é motivis de denguis. Atirei o pau no gatis, per gatis num morreus. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Sapien in monti palavris qui num significa nadis i pareci latim. ',
+      autoria: '',
+      modelo: 'modelo1',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
